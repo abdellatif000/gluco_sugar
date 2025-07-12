fc76187b3a42857ece07172cb1c6ae480d9e5738
Radouane FADEL <radouane.ai@gmail.com>
2025-07-12 03:05:07 +0000

    increase opacity for, header, sidenav, user details and Weight Managemen

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -98,7 +98,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-transparent md:block">
+      <div className="hidden border-r bg-glass md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold text-primary">
@@ -122,7 +122,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         </div>
       </div>
       <div className="flex flex-col bg-transparent">
-        <header className="flex h-14 items-center gap-4 border-b bg-transparent px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-14 items-center gap-4 border-b bg-glass px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button variant="outline" size="icon" className="shrink-0 md:hidden bg-transparent">
